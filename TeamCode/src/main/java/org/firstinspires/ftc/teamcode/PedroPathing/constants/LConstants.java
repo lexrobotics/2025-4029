823bdfd315f8b9d2fbc67584aebf49987280b9bf
Neil Chatterjee <139878980+Epikun07@users.noreply.github.com>
2025-01-24 16:33:33 -0500

    Lconstants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/constants/LConstants.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.PedroPathing.constants;
+
+
+import org.firstinspires.ftc.teamcode.PedroPathing.localization.Encoder;
+import org.firstinspires.ftc.teamcode.PedroPathing.localization.ThreeWheelLocalizer;
+
+
+public class LConstants {
+    static {
+        ThreeWheelLocalizer.FORWARD_TICKS_TO_INCHES = 0.00052; //lower it is, lower the telemetry value will be
+        ThreeWheelLocalizer.STRAFE_TICKS_TO_INCHES = 0.00052;
+        ThreeWheelLocalizer.TURN_TICKS_TO_RADIANS = 0.00052;
+        ThreeWheelLocalizer.leftY = 6.125;
+        ThreeWheelConstants.rightY = -6.125;
+        ThreeWheelConstants.strafeX = -4.4;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "leftRear";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
+        ThreeWheelLocalizer.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+    }
+}
+
+
+
+
